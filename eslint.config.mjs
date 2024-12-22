42b710a29260ba7168bf95a3ff589871368145e9
inca <boris@okunskiy.name>
2024-12-22 11:27:40 +0300

    chore: update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,12 +2,4 @@ import { sharedConfigs } from '@nodescript/eslint-config';
 
 export default [
     ...sharedConfigs,
-    {
-        rules: {
-            'vue/max-attributes-per-line': ['error', {
-                singleline: { max: 6 },
-                multiline: { max: 1 },
-            }]
-        }
-    }
 ];

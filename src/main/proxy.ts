d5b3aac1ffb84aa928aaccd89402181b5c0d34ad
inca <boris@okunskiy.name>
2024-12-23 00:17:46 +0300

    feat: add instance caching to proxy-based injection

--- src/main/proxy.ts
+++ src/main/proxy.ts
@@ -1,16 +1,25 @@
 import { Mesh, MESH_REF } from './mesh.js';
 import { ServiceKey } from './types.js';
 
+const PROXY_TRAPS = Object.getOwnPropertyNames(Reflect) as Array<keyof ProxyHandler<any>>;
+
 export type ServiceProxy<T> = T;
 
-export function dependency<T>(thisArg: any, serviceKey: ServiceKey<T>): T {
-    const handler = new Proxy({} as ProxyHandler<any>, {
-        get(_target, trap) {
-            return function (_target: any, ...args: any[]) {
-                const instance = (thisArg[MESH_REF] as Mesh).resolve(serviceKey);
-                return (Reflect as any)[trap](instance, ...args);
-            };
-        }
-    });
+export function dependency<T>(thisArg: any, serviceKey: ServiceKey<T>, cache = true): T {
+    let cachedInstance: T | undefined;
+    const handler: ProxyHandler<any> = {};
+    for (const trap of PROXY_TRAPS) {
+        handler[trap] = function (_target: any, ...args: any[]) {
+            let instance = cachedInstance;
+            if (!instance) {
+                instance = (thisArg[MESH_REF] as Mesh).resolve(serviceKey);
+            }
+            if (cache) {
+                cachedInstance = instance;
+            }
+            return (Reflect as any)[trap](instance, ...args);
+        };
+    }
+
     return new Proxy(thisArg, handler);
 }
